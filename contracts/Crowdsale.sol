98f8fcdfd0c7c3e85c75ce0148229f93e6f92ee7
ChrissyOs <chrissyoshields@gmail.com>
2023-09-14 23:11:50 -0400

    Buy Tokens pt. 1

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -1,7 +1,23 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "./Token.sol";
+
 contract Crowdsale {
-	string public name = "Crowdsale";
+	Token public token;
+	uint256 public price; 
+
+	constructor(
+		Token _token,
+		uint256 _price
+	) {
+		token = _token;
+		price = _price;
+	}
+
+	function buyTokens(uint256 _amount) public payable {
+		require(msg.value == (_amount / 1e18) * price); 
+		token.transfer(msg.sender, _amount);
+	}
 
 }
