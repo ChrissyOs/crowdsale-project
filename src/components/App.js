896c1c1c611cb5ac328427ef0dd84c0c726cd6fa
ChrissyOs <chrissyoshields@gmail.com>
2023-09-20 23:03:27 -0400

    Create Website pt. 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,31 @@
+import { useEffect } from "react"
+import { Container } from "react-bootstrap"
+import { ethers } from "ethers"
+
+// Components
+import Navigation from "./Navigation";
 
 function App() {
+
+	const loadBlockchainData = async () => {
+		const provider = new ethers.providers.Web3Provider(window.ethereum)
+		console.log(provider)
+
+		const accounts = await window.ethereum.request({ method: "eth_requestAccounts" })
+		const account = ethers.utils.getAddress(accounts[0])
+		console.log(account)
+	}
+
+	useEffect(() => {
+		loadBlockchainData()
+	});
+
+
 	return(
-		<div>Hello, world!</div>
-		)
+		<Container>
+			<Navigation />
+		</Container>
+	)
 }
 
 export default App;
